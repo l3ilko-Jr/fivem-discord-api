ad29c6ffd6c1060e7a693fa28535ee9af6864796
l3ilko Jr <54482782+l3ilko-Jr@users.noreply.github.com>
2020-02-26 11:23:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+const request = require('request');
 
+getAllServers = () => {
+  return new Promise(function(Success, Error) {
+    request('http://servers-live.fivem.net/api/servers/', function (error, response, body) {
+      if (error) return Error(error);
+      Success(JSON.parse(body));
+    });
+  });
+}
+
+getServerInfo = (ip) => {
+  return new Promise(function(Success, Error) {
+    request(`http://${ip}/info.json`, function (error, response, body) {
+      if (error) return Error(error);
+      info = JSON.parse(body);
+    });
+  });
+}
+
+getServerPlayers = (ip) => {
+  return new Promise(function(Success, Error) {
+    requesr(`http://${ip}/players.json`, function (error, response, body) {
+      if (error) return Error(error);
+      players = JSON.parse(body);
+    });
+  });
+}
+
+module.exports.getServerPlayers = getServerPlayers;
+module.exports.getServerInfo = getServerInfo;
