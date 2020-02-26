87b341fccfb84c9b7be358492a0e1e22f956ae7f
l3ilko Jr <54482782+l3ilko-Jr@users.noreply.github.com>
2020-02-26 12:46:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const data = require("data/function.js")
 
+runAll = (ip) => {
+  data.getAllServers();
+  data.getServerInfo(ip);
+  data.getServerPlayers(ip);
+}
+
+runPlayers = (ip) => {
+  data.getServerPlayers(ip);
+}
+
+runInfo = (ip) => {
+  data.getServerInfo(ip);
+}
+
+runAllServers = (ip) => {
+  data.getAllServers();
+}
+
+module.exports.runAll = runAll;
+module.exports.runPlayers = runPlayers;
+module.exports.runInfo = runInfo;
+module.exports.runAllServers = runAllServers;
